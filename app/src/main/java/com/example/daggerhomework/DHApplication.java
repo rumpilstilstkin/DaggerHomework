01fc3db6e2e5aa11c2bae366bd5b14670118cecd
semkina <semkina@yamoney.ru>
2019-02-13 19:01:49 +0300

    add leak...

---
+++ app/src/main/java/com/example/daggerhomework/DHApplication.java
@@ -0,0 +1,19 @@
+package com.example.daggerhomework;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+public class DHApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+    }
+}
